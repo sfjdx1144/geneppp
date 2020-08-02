e892873a84e5ac03a3fa7ef85023f929f79bbd0c
sfjdx1144 <sfjdx1144@live.com>
2020-08-02 21:25:36 +0800

    Update convert.js

--- convert.js
+++ convert.js
@@ -13,5 +13,19 @@ console.log(geneID)
 window.location.href="http://solanaceae.plantbiology.msu.edu/cgi-bin/annotation_report.cgi?gene_id="+geneID+"&Submit=Submit";
 }
 
+var url = "gene.json"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+            var request = new XMLHttpRequest();
+            request.open("get", url);/*设置请求方法与路径*/
+            request.send(null);/*不发送数据到服务器*/
+            request.onload = function () {/*XHR对象获取到返回信息后执行*/
+                if (request.status == 200) {/*返回状态为200，即为数据获取成功*/
+                    var json = JSON.parse(request.responseText);
+                    for(var i=0;i<json.length;i++){
+                    	console.log(json[i].name);
+                    }
+                    console.log(json);
+                }
+            }
+
 
 
