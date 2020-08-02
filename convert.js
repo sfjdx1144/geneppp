adb964787746928f1dab279183d68fa2d1281b0b
sfjdx1144 <sfjdx1144@live.com>
2020-08-02 18:58:42 +0800

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,13 @@
+function fun(gene){
+        var gene_arr ={
+'StPUB1':'PGSC0003DMG400022775',
+'StPUB2':'PGSC0003DMG400032151',
+'StPUB3':'PGSC0003DMG400021360',
+'StPUB4':'PGSC0003DMG400021359',
+'StPUB5':'PGSC0003DMG400021358'
+}
+var geneID=gene_arr[gene]
+console.log();
+console.log(geneID)
+window.location.href="http://solanaceae.plantbiology.msu.edu/cgi-bin/annotation_report.cgi?gene_id="+geneID+"&Submit=Submit";
+}
