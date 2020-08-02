9aaa0a205b7ac2880468fc589a45876a538fb5e8
sfjdx1144 <sfjdx1144@live.com>
2020-08-02 22:39:37 +0800

    Update convert.js

--- convert.js
+++ convert.js
@@ -17,6 +17,46 @@ function fun(gene_h){
 
 var geneStr
 
+function task1 () {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      var url = "gene.csv"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
+      var request = new XMLHttpRequest();
+      request.open("get", url);/*设置请求方法与路径*/
+      request.send(null);/*不发送数据到服务器*/
+      request.onload = function () {/*XHR对象获取到返回信息后执行*/
+      geneStr=request.responseText;
+      }
+      resolve('done');
+    }, 1000);
+  });
+}
+
+function task2 () {
+
+  return new Promise(resolve => {
+    setTimeout(() => {
+      console.log('2', '第二个任务');
+      
+          var gene=geneStr.split('\n')
+    for(i=0;i<gene.length;i++){
+      var genetemp=gene[i].split(',');
+      geneName.unshift(genetemp[0]);
+      geneID.unshift(genetemp[1]);
+    }
+      
+      
+      resolve('done');
+    }, 1000)
+  });
+}
+
+
+
+async function allTasks () {
+  await task1();
+  await task2();
+}
 
             
 console.log(geneStr)
@@ -24,19 +64,8 @@ console.log(geneStr)
 geneReq()
 fun()
 function geneReq(){
-    var url = "gene.csv"/*json文件url，本地的就写本地的位置，如果是服务器的就写服务器的路径*/
-            var request = new XMLHttpRequest();
-            request.open("get", url);/*设置请求方法与路径*/
-            request.send(null);/*不发送数据到服务器*/
-            request.onload = function () {/*XHR对象获取到返回信息后执行*/
-                    geneStr=request.responseText;
-                }
-    var gene=geneStr.split('\n')
-    for(i=0;i<gene.length;i++){
-      var genetemp=gene[i].split(',');
-      geneName.unshift(genetemp[0]);
-      geneID.unshift(genetemp[1]);
-    }
+    
+
 }
 
 
